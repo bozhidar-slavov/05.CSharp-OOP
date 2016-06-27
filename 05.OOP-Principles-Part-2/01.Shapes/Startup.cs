c0a05d5269da1c724df50b355e003753861f28c4
bozhidar-slavov <bozhidar.slavov96@gmail.com>
2016-06-27 15:53:18 +0300

    OOP principles part 2 task added!

---
+++ 05.OOP-Principles-Part-2/01.Shapes/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Shape[] differentShapes =
+            {
+                new Rectangle(4.5, 6.5),
+                new Rectangle(4, 5.5),
+                new Triangle(5.8, 3),
+                new Triangle(4.5, 8.5),
+                new Square(4)
+            };
+
+            foreach (var shape in differentShapes)
+            {
+                Console.WriteLine("{0} Area --> {1:F2}", shape.GetType().Name, shape.CalculateSurface());
+            }
+        }
+    }
+}
---
+++ 05.OOP-Principles-Part-2/03.RangeExceptions/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace RangeExceptions
+{
+    using System;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            // test with integer
+            try
+            {
+                Console.Write("Enter integer number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0 || number > 100)
+                {
+                    throw new InvalidRangeException<int>(1, 100);
+                }
+            }
+            catch (InvalidRangeException<int> exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.WriteLine();
+
+            // test with DateTime
+
+            var testDate = new DateTime(2016, 3, 20); // change for another test
+
+            var startDate = new DateTime(1980, 1, 1);
+            var endDate = new DateTime(2013, 12, 31);
+
+            Console.WriteLine("Test date --> Year: {0} Month: {1} Day: {2}", testDate.Year, testDate.Month, testDate.Day);
+            try
+            {
+                if (testDate < startDate || testDate > endDate)
+                {
+                    throw new InvalidRangeException<DateTime>(startDate, endDate);
+                }
+            }
+            catch (InvalidRangeException<DateTime> exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
