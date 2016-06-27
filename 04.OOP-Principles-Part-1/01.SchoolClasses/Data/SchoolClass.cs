4bebdbda9f409738fb86c737d0dd5d4d5a66d6d3
bozhidar-slavov <bozhidar.slavov96@gmail.com>
2016-06-27 14:11:49 +0300

    Fixed task

--- 04.OOP-Principles-Part-1/01.SchoolClasses/Data/SchoolClass.cs
+++ 04.OOP-Principles-Part-1/01.SchoolClasses/Data/SchoolClass.cs
@@ -9,9 +9,6 @@
 
         public char ClassId { get; private set; }
 
-        private List<Teacher> teachers;
-        private List<Student> students;
-
         public SchoolClass(List<Teacher> teachers, List<Student> students, string comment = null)
         {
             this.ClassId = autoId;
@@ -22,39 +19,17 @@
             autoId++;
         }
 
-        public List<Teacher> Teachers
-        {
-            get
-            {
-                return this.teachers;
-            }
+        public List<Teacher> Teachers { get; private set; }
 
-            set
-            {
-                this.teachers = value;
-            }
-        }
+        public List<Student> Students { get; private set; }
 
-        public List<Student> Students
-        {
-            get
-            {
-                return this.students;
-            }
-
-            set
-            {
-                this.students = value;
-            }
-        }
-
-        public string Comment { get; set; }
+        public string Comment { get; private set; }
 
         public string GetDisciplines()
         {
-            HashSet<string> classDisciplines = new HashSet<string>();
+            var classDisciplines = new HashSet<string>();
 
-            foreach (var teacher in this.teachers)
+            foreach (var teacher in this.Teachers)
             {
                 foreach (var discipline in teacher.Disciplines)
                 {
@@ -71,14 +46,13 @@
 
             result.AppendLine(string.Format("Class: {0}", this.ClassId));
             result.AppendLine("Teachers: ");
-            result.AppendLine(string.Join(", ", this.teachers) + "\n");
+            result.AppendLine(string.Join(", ", this.Teachers) + "\n");
             result.AppendLine("Students: ");
-            result.AppendLine(string.Join(", ", this.students) + "\n");
+            result.AppendLine(string.Join(", ", this.Students) + "\n");
             result.AppendLine("Disciplines: ");
             result.AppendLine(this.GetDisciplines());
 
             return result.ToString();
-
         }
 
         public void MakeComment(string text)
